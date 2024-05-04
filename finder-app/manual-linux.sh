1bfb95065e5347e7fa4c2f2ea51f9db9bce18ea9
gpcm <gianpcm@gmail.com>
2024-05-04 01:11:48 -0700

    Fixing errors

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -97,7 +97,7 @@ sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
 sudo mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # TODO: Clean and build the writer utility
-cd ~/Documents/Linux-Course/assignments/finder-app
+cd ${FINDER_APP_DIR}
 make clean
 make CROSS_COMPILE=${CROSS_COMPILE}
 
