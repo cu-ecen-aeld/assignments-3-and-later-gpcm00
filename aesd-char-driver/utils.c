b2976c8369d6116741c4d45560106aa15337c881
gpcm <gianpcm@gmail.com>
2024-06-04 22:06:01 -0700

    no utils

--- aesd-char-driver/utils.c
+++
@@ -1,7 +0,0 @@
-#ifdef __KERNEL__
-#include <linux/types.h>
-#else
-#include <stddef.h> // size_t
-#include <stdint.h> // uintx_t
-#include <stdbool.h>
-#endif
--- aesd-char-driver/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-size_t scan_newline(char* str, size_t len);
-
-#endif
